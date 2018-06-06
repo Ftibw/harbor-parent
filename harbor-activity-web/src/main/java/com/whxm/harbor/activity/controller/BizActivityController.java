7b23eef24fdb151aae0af09bdbfd991d6a095136
Ftibw <ftibw@live.com>
2018-06-06 23:42:30 +0800

    配置api注解提供restful接口

--- harbor-activity-web/src/main/java/com/whxm/harbor/activity/controller/BizActivityController.java
+++ harbor-activity-web/src/main/java/com/whxm/harbor/activity/controller/BizActivityController.java
@@ -17,12 +17,9 @@ public class BizActivityController {
     @Autowired
     private ActivityService activityService;
 
-    /**
-     * 获取楼层列表
-     */
     @ApiOperation("获取楼层列表")
     @GetMapping("/bizActivities")
-    public Result getBizActivities(){
+    public Result getBizActivities() {
         List<BizActivity> bizActivityList = activityService.getBizActivityList();
         Result ret = new Result();
         ret.setData(bizActivityList);
@@ -31,41 +28,29 @@ public class BizActivityController {
         return ret;
     }
 
-    /**
-     * 修改楼层
-     */
     @ApiOperation("修改楼层")
     @PutMapping("/bizActivity/")
-    public Result updateBizActivity(@RequestBody BizActivity bizActivity){
+    public Result updateBizActivity(@RequestBody BizActivity bizActivity) {
         Result result = activityService.updateBizActivity(bizActivity);
         return result;
     }
 
-    /**
-     * 删除楼层
-     */
     @ApiOperation("删除楼层")
     @DeleteMapping("/bizActivity/{ID}")
-    public Result delBizActivity(@ApiParam(name = "ID",value = "楼层的ID",required = true)
-                                     @PathVariable("ID") Integer bizActivityId){
+    public Result delBizActivity(@ApiParam(name = "ID", value = "楼层的ID", required = true)
+                                 @PathVariable("ID") Integer bizActivityId) {
         Result result = activityService.deleteBizActivity(bizActivityId);
         return result;
     }
 
-    /**
-     * 添加楼层
-     */
     @ApiOperation("添加楼层")
     @PostMapping("/bizActivity/")
-    public Result addBizActivity(@RequestBody BizActivity bizActivity){
+    public Result addBizActivity(@RequestBody BizActivity bizActivity) {
         Result result = activityService.addBizActivity(bizActivity);
         return result;
     }
 
-    /**
-     * 上传logo
-     */
-    public Result uploadLogo(){
+    public Result uploadLogo() {
         return null;
     }
 }
--- harbor-business-web/src/main/java/com/whxm/harbor/business/controller/BusinessFormatController.java
+++ harbor-business-web/src/main/java/com/whxm/harbor/business/controller/BusinessFormatController.java
@@ -4,6 +4,8 @@ import com.whxm.harbor.bean.BizFormat;
 import com.whxm.harbor.business.service.BusinessFormatService;
 import com.whxm.harbor.common.bean.Result;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +21,9 @@ public class BusinessFormatController {
     @Autowired
     private BusinessFormatService businessFormatService;
 
+    @ApiOperation("获取业态列表")
     @GetMapping("/bizFormats")
-    public Result getBizFormats(){
+    public Result getBizFormats() {
         List<BizFormat> bizFormatList = businessFormatService.getBizFormatList();
         Result ret = new Result();
         ret.setData(bizFormatList);
@@ -29,8 +32,9 @@ public class BusinessFormatController {
         return ret;
     }
 
+    @ApiOperation(value = "根据首字母获取业态列表", notes = "首字母不区分大小写")
     @GetMapping("/bizFormats/{keyword}")
-    public Result getBizFormats(@PathVariable("keyword") String keyword){
+    public Result getBizFormats(@PathVariable("keyword") String keyword) {
         List<BizFormat> bizFormatList = businessFormatService.getBizFormatByNumPrefix(keyword);
         Result ret = new Result();
         ret.setData(bizFormatList);
@@ -39,21 +43,24 @@ public class BusinessFormatController {
         return ret;
     }
 
-
+    @ApiOperation("新增业态")
     @PostMapping("/bizFormat/")
-    public Result addBizFormat(@RequestBody BizFormat bizFormat){
+    public Result addBizFormat(@RequestBody BizFormat bizFormat) {
         Result result = businessFormatService.addBizFormat(bizFormat);
         return result;
     }
 
+    @ApiOperation("修改业态")
     @PutMapping("/bizFormat/")
-    public Result updateBizFormat(@RequestBody BizFormat bizFormat){
+    public Result updateBizFormat(@RequestBody BizFormat bizFormat) {
         Result result = businessFormatService.updateBizFormat(bizFormat);
         return result;
     }
 
+    @ApiOperation("删除业态")
     @DeleteMapping("/bizFormat/{ID}")
-    public Result delBizFormat(@PathVariable("ID") String bizFormatId){
+    public Result delBizFormat(@ApiParam(name = "ID", value = "业态ID", required = true)
+                               @PathVariable("ID") String bizFormatId) {
         Result result = businessFormatService.deleteBizFormat(bizFormatId);
         return result;
     }
