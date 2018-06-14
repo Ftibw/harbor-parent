f4d833d8aa757d773e7c231938200d4f0c635095
Ftibw <ftibw@live.com>
2018-06-14 11:00:24 +0800

    update

---
+++ harbor-common/src/main/java/com/whxm/harbor/bean/ResultMap.java
@@ -0,0 +1,15 @@
+package com.whxm.harbor.bean;
+
+import java.util.HashMap;
+
+public class ResultMap<K, V> extends HashMap<K, V> {
+
+    public ResultMap(int initialCapacity) {
+        super(initialCapacity);
+    }
+
+    public ResultMap<K, V> build(K key, V value) {
+        this.put(key, value);
+        return this;
+    }
+}
